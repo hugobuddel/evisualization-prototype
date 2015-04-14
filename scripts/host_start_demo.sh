ba144d27bd71eda2bc0b5c8ced28314bcdd4a7db
pnoorishad <pari.noorishad@gmail.com>
2015-04-14 13:18:39 +0200

    we fixed shebang

--- scripts/host_start_demo.sh
+++ scripts/host_start_demo.sh
@@ -1,11 +1,11 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 # Directory of the script
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Get settings if necessary.
-$DIR/lxc_settings_auto.sh
+source $DIR/lxc_settings_auto.sh
 
 # Set the system and awe user if necessary.
 $DIR/host_create_user.sh
--- scripts/lxc_settings_auto.sh
+++ scripts/lxc_settings_auto.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 # Automatically determine the best lxc settings.
 
