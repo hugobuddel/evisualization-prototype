132e9a64fd686b8a74843b95b9e12470cd76899e
pnoorishad <pari.noorishad@gmail.com>
2015-04-28 13:23:35 +0200

    these are not necessary any longer

--- scripts/user_orange_astropy.sh
+++
@@ -1,6 +0,0 @@
-# Astropy
-git clone https://github.com/astropy/astropy.git
-pushd astropy
-python setup.py build
-python setup.py install
-popd
--- scripts/user_orange_virtualenv.sh
+++
@@ -1,9 +0,0 @@
-virtualenv -p python3 --system-site-packages --distribute orange3env
-. orange3env/bin/activate
-
-# Dependencies
-
-pip install numpy>=1.9.0
-pip install scikit-learn
-pip install pyqtgraph
-
