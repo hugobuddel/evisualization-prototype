a4ece21563763c729b726a4e6e7265073fda16ea
pnoorishad <pari.noorishad@gmail.com>
2015-04-28 14:15:27 +0200

    anaconda installation

---
+++ scripts/user_install_anaconda.sh
@@ -0,0 +1,8 @@
+# to install Anaconda python3 which has all packages that we need
+
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.2.0-Linux-x86_64.sh
+
+bash Anaconda3-2.2.0-Linux-x86_64.sh
+
+export PATH=/home/evis/anaconda3:\${PATH}
+echo "export PATH=/home/evis/anaconda3:\${PATH}" >> ~/.bashrc
