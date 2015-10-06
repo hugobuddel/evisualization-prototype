d54b6abe5dced9da843ddfee62e8c0276b6aac04
hugobuddel <hugo@buddelmeijer.nl>
2015-10-06 11:53:18 +0200

    add git link

--- scripts/user_install_orange.sh
+++ scripts/user_install_orange.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/hugobuddel/orange3.git
+#git clone git://github.com/hugobuddel/orange3.git
 pushd orange3
 git remote add upstream https://github.com/biolab/orange3.git
 git fetch upstream
