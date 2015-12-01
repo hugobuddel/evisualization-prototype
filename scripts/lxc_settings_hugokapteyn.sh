0a16524aea5eb1da133fa7bcc64cd13b84ee0d2d
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2015-12-01 11:39:33 +0100

    Cleanup of scripts.

--- scripts/lxc_settings_hugokapteyn.sh
+++
@@ -1,5 +0,0 @@
-export DIR_EVIS=./
-export DIR_LXC_EVIS=$DIR_EVIS/lxc_evis
-
-# Need to get dir?
-source lxc_settings.sh
--- scripts/start_awe_demo.py
+++ scripts/start_awe_demo.py
@@ -3,27 +3,34 @@ from astro.main.sourcecollection.SourceCollectionTree import SourceCollectionTre
 
 import os
 
-Env['loglevel'] = 63
+# Set the loglevel to a binary mask for more logging.
+#Env['loglevel'] = 63
+
+# The commented-out code below is relevant to demo pulling of expensive
+# attributes, which is still experimental.
 
 # Overload the function to sort new AttributeCalculators to give
 # preference to the photometric redshift calculator on the CVS.
-from astro.main.sourcecollection.SourceCollectionTree import key_find_attribute_new_calculators
-def my_key_find_attribute_new_calculators(scd):
-    """
-    Rank such that the file based ACDs are ranked higher.
-    """
-    tkey = key_find_attribute_new_calculators(scd)
-    if 'PYTHON:' in scd['sc'].definition.filename:
-        tkey += 10
-    else:
-        tkey -= 10
-    
-    return tkey
+#from astro.main.sourcecollection.SourceCollectionTree import key_find_attribute_new_calculators
+#def my_key_find_attribute_new_calculators(scd):
+#    """
+#    Rank such that the file based ACDs are ranked higher.
+#    """
+#    tkey = key_find_attribute_new_calculators(scd)
+#    if 'PYTHON:' in scd['sc'].definition.filename:
+#        tkey += 10
+#    else:
+#        tkey -= 10
+#    
+#    return tkey
 
-SourceCollectionTree.key_functions['find_attribute_new_calculators'] = my_key_find_attribute_new_calculators
+#SourceCollectionTree.key_functions['find_attribute_new_calculators'] = my_key_find_attribute_new_calculators
 
-os.environ["BPZPATH"] = "/home/evis/BPZ/"
+#os.environ["BPZPATH"] = "/home/evis/BPZ/"
 
-context.set_project("KIDS")
+#context.set_project("KIDS")
 
 samp = Samp()
+samp.use_onthefly = False
+samp.broadcast_sourcecollections_in_parts = True
+
