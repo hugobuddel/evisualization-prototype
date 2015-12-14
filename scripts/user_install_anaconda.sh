03c76203463fd068719dd7f5ec317dabb237892a
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2015-12-14 12:11:39 +0100

    Use miniconda instead of anaconda.

--- scripts/user_install_anaconda.sh
+++ scripts/user_install_anaconda.sh
@@ -1,11 +1,13 @@
 # to install Anaconda python3 which has all packages that we need
 
 #wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.2.0-Linux-x86_64.sh
-wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.3.0-Linux-x86_64.sh
+#wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.3.0-Linux-x86_64.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 
 #bash Anaconda3-2.2.0-Linux-x86_64.sh
-bash Anaconda3-2.3.0-Linux-x86_64.sh -b
+#bash Anaconda3-2.3.0-Linux-x86_64.sh -b
+bash miniconda.sh -b -p /home/evis/miniconda
 
-# TODO: Ensure this PATH is also set during installation.
+# Soure ~/.bashrc to get this path also during installation.
 export PATH=/home/evis/anaconda3/bin:${PATH}
-echo "export PATH=/home/evis/anaconda3/bin:\${PATH}" >> ~/.bashrc
+echo "export PATH=/home/evis/miniconda/bin:\${PATH}" >> ~/.bashrc
