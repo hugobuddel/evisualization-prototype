af5dc012fbddcb678f1153e7650842d00f43a823
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2015-12-14 12:45:37 +0100

    Do not hardcode path.

--- scripts/get_prototype_preinstalled.sh
+++ scripts/get_prototype_preinstalled.sh
@@ -1,27 +1,32 @@
+#!/usr/bin/env bash
+
 # Get a preinstalled version of the eVisualization prototype.
 
+# Directory of the script
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 # Get proot, needs to be available outside of the container.
-scripts/get_proot.sh 
+$DIR/get_proot.sh 
 
 # Get the container.
-scripts/get_container_preinstalled.sh
+$DIR/get_container_preinstalled.sh
 
 # Create a user in the container. This is necessary to ensure the userid matches the files.
-scripts/host_create_user.sh
+$DIR/host_create_user.sh
 
 # Copy these scripts to the container.
-scripts/copy_scripts.sh
+$DIR/copy_scripts.sh
 
 # Tell the shell where the container can be found and make shell functions.
-source scripts/lxc_settings.sh
+source $DIR/lxc_settings.sh
 
 # Update the prototype code.
 evis_user /home/evis/scripts/user_update_aweorange.sh
 
 # Copy the awe user if necessary.
-scripts/host_copy_awe_user.sh
+$DIR/host_copy_awe_user.sh
 
 # Update the system packages.
-evis_root /home/evis/scripts/root_aptget_update.sh
 evis_root /home/evis/scripts/root_aptget_upgrade.sh
 
