a32e48a299aed71206ceb5480b562833e2b39999
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2015-12-14 12:12:31 +0100

    Cleanup.

--- scripts/user_install_orange.sh
+++ scripts/user_install_orange.sh
@@ -1,16 +1,12 @@
 git clone https://github.com/hugobuddel/orange3.git
 #git clone git@github.com:hugobuddel/orange3.git
 pushd orange3
-git remote add upstream https://github.com/biolab/orange3.git
-git fetch upstream
-#git checkout master
-#git checkout upstream/master
-# pip does not work well with anaconda, the requirements are
-# installed using conda in user_orange_dependencies.sh
+#git remote add upstream https://github.com/biolab/orange3.git
+#git fetch upstream
+# installing the requirements through pip should work, but they should
+# already be installed using conda in user_orange_dependencies.sh
 #pip install -r requirements.txt
-# Use develop for the testing release.
 python3 setup.py develop
-#git checkout develop
 popd
 
 # Create the large test dataset for the LazyFile widget.
